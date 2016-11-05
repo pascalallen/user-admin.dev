7dc47abf22665d56d5480befb60c3bc0c91cb605
pascalallen <thomaspascalallen@yahoo.com>
2016-11-05 15:27:35 -0500

    added font awesome dependency

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,17 +7,14 @@
 	    <title>User/Admin Portal</title>
 
 		{{-- BOOTSTRAP CSS --}}
-		<link href="/css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
 
 		{{-- CUSTOM CSS --}}
-		<link rel="stylesheet" type="text/css" href="/css/main.css">
-
-		{{-- CUSTOM FONT --}}
-		<link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" type="text/css" href="css/main.css">
 
 		{{-- FONT AWESOME --}}
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-		
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+
 		@yield('top-script')
 	</head>
 	<body>
