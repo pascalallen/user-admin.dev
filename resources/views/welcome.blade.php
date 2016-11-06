edbf956e14ed08420b7d542167a50c290a24dce2
pascalallen <thomaspascalallen@yahoo.com>
2016-11-06 14:04:58 -0600

    Fixed form builder. Added admin column to users table to authenticate general users and also users that are admins

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,19 +19,19 @@
 
     <div class="row">
         <div class="col-xs-4 col-xs-offset-4 login">
-            {{ Form::open(array('action' => 'HomeController@postLogin')) }}
-                <div class="{{ ($errors->has('email')) ? 'has-error' : '' }} form-group">
-                    {{ $errors->first('email', '<div class="alert alert-danger">:message</div>') }}
-                    {{ Form::label('email', 'Email') }}
-                    {{ Form::email('email', Input::old('email'), ['class' => 'form-control', 'placeholder'=>'Email']) }}
+            {!! Form::open(array('action' => 'HomeController@postLogin')) !!}
+                <div class="{!! ($errors->has('email')) ? 'has-error' : '' !!} form-group">
+                    {!! $errors->first('email', '<div class="alert alert-danger">:message</div>') !!}
+                    {!! Form::label('email', 'Email') !!}
+                    {!! Form::email('email', Input::old('email'), ['class' => 'form-control', 'placeholder'=>'Email']) !!}
                 </div>
-                <div class="{{ ($errors->has('password')) ? 'has-error' : '' }} form-group">
-                    {{ $errors->first('password', '<div class="alert alert-danger">:message</div>') }}
-                    {{ Form::label('password', 'Password') }}
-                    {{ Form::password('password', ['class' => 'form-control', 'placeholder'=>'Password']) }}
+                <div class="{!! ($errors->has('password')) ? 'has-error' : '' !!} form-group">
+                    {!! $errors->first('password', '<div class="alert alert-danger">:message</div>') !!}
+                    {!! Form::label('password', 'Password') !!}
+                    {!! Form::password('password', ['class' => 'form-control', 'placeholder'=>'Password']) !!}
                 </div>
                 <button type="submit" class="btn btn-default">Submit</button>
-            {{ Form::close() }}
+            {!! Form::close() !!}
         </div>
     </div>
 
